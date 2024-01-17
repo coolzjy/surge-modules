392df23bb362f5d4b1dc5f828b08e15810b6ac9c
Zhang Visper <reg@zjy.name>
2024-01-17 12:15:38 +0800

    Update index.js

--- unlock-duolingo/index.js
+++ unlock-duolingo/index.js
@@ -5,20 +5,24 @@ for (let i = 0; i < responses.length; i++) {
   const response = responses[i];
   const { status, body: raw } = response;
 
-  if (status !== 200 || raw == null || raw === "") continue;
-
+  if (status !== 200 || raw == null || raw === "" || !raw.includes("plusStatus")) continue;
+  
   const body = JSON.parse(raw);
 
+  // enable duolingo plus
   if (body.plusStatus === "FREE") {
     body.plusStatus = "PLUS";
   }
 
-  if (Array.isArray(body.privacySettings)) {
-    body.privacySettings = [];
+  // enable unlimit hearts
+  if (body.health != null) {
+    body.health.unlimitedHeartsAvailable = true
   }
 
+  // fill timer boost
   if (body.timerBoostConfig != null) {
-    body.timerBoostConfig.timerBoosts = 10;
+    body.timerBoostConfig.timerBoosts = 1;
+    body.timerBoostConfig.timePerBoost = 120;
   }
 
   response.body = JSON.stringify(body);
