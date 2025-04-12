b2ae906b804f7e6e88f5e7cf4c88ff0f6c2033cc
Zhang Visper <reg@zjy.name>
2025-04-12 22:00:52 +0800

    Update super.js

--- duolingo/super.js
+++ duolingo/super.js
@@ -1,14 +1,1 @@
-const body = JSON.parse($response.body);
-const responses = body.responses;
-
-for (let i = 0; i < responses.length; i++) {
-  const response = responses[i];
-  const { status, body: raw } = response;
-
-  if (status !== 200 || raw == null || raw === "" || !raw.includes("subscriberLevel")) continue;
-
-  // enable duolingo plus
-  response.body = raw.replace('FREE', 'PREMIUM')
-}
-
-$done({ body: JSON.stringify(body) });
+$done({ body: $response.body.replace('FREE', 'PREMIUM') });
